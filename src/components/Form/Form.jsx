eae9647ed525db7b21744ef0b442da2846d369bb
Vadim Vladimirov <kurtonchik@gmail.com>
2022-07-29 13:38:13 +0300

    start form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,97 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { FormWrapper, FormTitle, FormStyled, ButtonStyled } from './Form.styled';
 
 const Form = () => {
+  const positions = [
+    {
+      id: 1,
+      name: 'Lawyer',
+    },
+    {
+      id: 2,
+      name: 'Content manager',
+    },
+    {
+      id: 3,
+      name: 'Security',
+    },
+    {
+      id: 4,
+      name: 'Designer',
+    },
+  ];
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+      position: '',
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      phone: Yup.string(),
+      position: Yup.string(),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm({ values: '' });
+    },
+  });
+
   return (
-    <div>
-      <h3>Working with POST request</h3>
-      <form></form>
-    </div>
+    <FormWrapper>
+      <FormTitle>Working with POST request</FormTitle>
+      <FormStyled onSubmit={formik.handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Your name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        <input
+          name="phone"
+          type="tel"
+          placeholder="Phone"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.phone}
+        />
+        <p>Select your position</p>
+        {positions.map(position => (
+          <label key={position.id}>
+            <input
+              type="radio"
+              name="position"
+              checked={formik.values.position === position.name}
+              value={position.name}
+              onChange={formik.handleChange}
+            />
+            {position.name}
+          </label>
+        ))}
+        <div>
+          <input type="file" id="photo" name="photo" accept="image/*" />
+          <label htmlFor="photo">
+            <span>Upload</span>
+            <span>Upload your photo</span>
+          </label>
+        </div>
+        <ButtonStyled type="submit">Sign up</ButtonStyled>
+      </FormStyled>
+    </FormWrapper>
   );
 };
 
---
+++ src/components/Form/Form.styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const FormWrapper = styled.div`
+  border: 1px solid red;
+  text-align: center;
+  padding-bottom: 100px;
+`;
+
+export const FormTitle = styled.h3`
+  font-size: 40px;
+  line-height: 40px;
+  color: rgba(0, 0, 0, 0.87);
+  margin-bottom: 50px;
+`;
+
+export const FormStyled = styled.form`
+  border: 1px solid red;
+  max-width: 380px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const ButtonStyled = styled.button`
+  width: 100px;
+  border-radius: 80px;
+  background: #f4e041;
+  padding: 4px;
+  text-align: center;
+  font-size: 16px;
+  line-height: 26px;
+  color: rgba(0, 0, 0, 0.87);
+`;
